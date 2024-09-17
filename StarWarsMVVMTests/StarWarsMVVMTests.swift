ecc5bcba322493aba6dc4bd85491eeea13dd47a5
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2024-09-16 20:34:02 -0500

    Crear testing con Swift Testing

--- StarWarsMVVMTests/StarWarsMVVMTests.swift
+++ StarWarsMVVMTests/StarWarsMVVMTests.swift
@@ -6,11 +6,26 @@
 //
 
 import Testing
+@testable import StarWarsMVVM
 
+extension Tag {
+    @Tag static var repository: Self
+}
+
+@Suite("Prueba del repository del Star Cards", .tags(.repository))
 struct StarWarsMVVMTests {
+    let repository = RepositoryTest()
+    let viewModel = StarCardViewModel(repository: RepositoryTest())
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test("Prueba de carga de datos del repositorio")
+    func dataLoad() throws {
+        let data = try repository.getData()
+        #expect(data.count == 4)
+    }
+    
+    @Test("Prueba de carga de datos del ViewModel")
+    func dataLoadVM() throws {
+        #expect(viewModel.cards.count == 4)
     }
 
 }
