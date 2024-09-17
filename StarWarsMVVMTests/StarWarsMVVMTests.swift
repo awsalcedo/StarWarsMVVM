9efca780e3cae25fa49a1ea2de48cac9b0d77d6c
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2024-09-16 20:33:30 -0500

    Crear test con Swift Testing

---
+++ StarWarsMVVMTests/StarWarsMVVMTests.swift
@@ -0,0 +1,16 @@
+//
+//  StarWarsMVVMTests.swift
+//  StarWarsMVVMTests
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 16/9/24.
+//
+
+import Testing
+
+struct StarWarsMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
